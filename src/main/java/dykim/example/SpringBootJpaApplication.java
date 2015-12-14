f5f20b22ec940ffab88d79b9ab25c842156db63f
kimdoyong <kdy10041004@naver.com>
2015-12-14 16:13:37 +0900

    init

--- src/main/java/dykim/example/SpringBootJpaApplication.java
+++ src/main/java/dykim/example/SpringBootJpaApplication.java
@@ -8,6 +8,5 @@ public class SpringBootJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJpaApplication.class, args);
-
     }
 }
---
+++ src/main/java/dykim/example/index/IndexController.java
@@ -0,0 +1,17 @@
+package dykim.example.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by kimdoyong on 2015. 12. 14..
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("name", "Spring Boot Start");
+        return "index";
+    }
+}
