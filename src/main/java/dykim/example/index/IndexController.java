58f813257855cc0e8b4d2dd77662dd6cb4b37321
kimdoyong <kdy10041004@naver.com>
2015-12-15 12:45:00 +0900

    init

--- src/main/java/dykim/example/index/IndexController.java
+++ src/main/java/dykim/example/index/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 /**
  * Created by kimdoyong on 2015. 12. 14..
  */
