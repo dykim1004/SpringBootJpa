ab58a91f76755ea33617a9d40b0aa825a64f9568
Administrator <Administrator@WIN-DA778UOEB3E>
2015-12-21 16:03:17 +0900

    Security 임시 모두 허용

---
+++ src/main/java/dykim/example/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package dykim.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	// 01. 인메모리 로그인
+	/*@Override
+	public void configure(AuthenticationManagerBuilder auth)throws Exception{
+		auth.inMemoryAuthentication().withUser("admin").password("1234").roles("ADMIN");
+	}*/
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		//일단 허용
+		httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+		// static resources
+		//httpSecurity.authorizeRequests()
+		//.antMatchers("/css/**", "/js/**", "/images/**", "/resources/**", "/webjars/**").permitAll();
+		
+		/*httpSecurity.authorizeRequests()
+						.antMatchers("/signin").anonymous()
+						.anyRequest().authenticated()
+						.and()
+					.formLogin()
+						.loginPage("/signin")
+						.loginProcessingUrl("/sign-in-process.html")
+						.failureUrl("/signin?error")
+						.usernameParameter("username")
+						.passwordParameter("password")
+						.defaultSuccessUrl("/admin/dashboard.html", true)
+						.and()
+					.logout()
+						.logoutSuccessUrl("/signin?logout");
+		
+		httpSecurity.exceptionHandling().accessDeniedPage("/admin/dashboard.html");
+		httpSecurity.sessionManagement().invalidSessionUrl("/signin");*/
+		
+	}
+	
+}
