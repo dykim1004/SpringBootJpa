88a24790ee87dece962389858122a5b1887fb07a
kimdoyong <kdy10041004@naver.com>
2016-01-03 13:47:33 +0900

    editor 추가

--- src/main/java/dykim/example/blog/vo/Post.java
+++ src/main/java/dykim/example/blog/vo/Post.java
@@ -7,27 +7,34 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 /**
  * Created by kimdoyong on 2016. 1. 2..
  */
+@Getter
+@Setter
 @Entity
 public class Post {
     @Id
     @GeneratedValue
-    @Getter @Setter
     int id;
-    @Getter @Setter
-    String subject;
 
-    @Column(length = 100000000)
-    @Getter @Setter
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(nullable = false)
+    String title;
+
+    @Size(max = 255)
+    String subtitle;
+
+    @NotNull
+    @Size(min = 1, max = 100000000)
+    @Column(length = 100000000, nullable = false)
     String content;
 
-    @Getter @Setter
     Date regDate;
-
-
 
 }
